56288fde71c4acaab0266a796b3e7651f11d3823
Robin Rolf <imer@imer.cc>
2023-08-27 15:24:57 +0200

    Multi-Project Summaries
    To make working with data easier since grouping is a pain

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fs::File;
 use std::process::exit;
 use std::sync::Arc;
 use std::time::Duration;
@@ -13,7 +14,7 @@ use reqwest::Client as HttpClient;
 use reqwest::ClientBuilder;
 use serde::Deserialize;
 use tokio::{select, signal};
-use std::fs::File;
+use tokio::sync::Mutex;
 use tokio::task::JoinSet;
 use tokio::time::{Instant, sleep};
 use tokio_util::sync::CancellationToken;
@@ -23,6 +24,8 @@ use tokio_util::sync::CancellationToken;
 struct Config {
     /// This specifies which projects to track
     pub projects: Vec<String>,
+    /// This specifies which summaries from multiple projects to create, summary-all is auto-created
+    pub summaries: HashMap<String, Vec<String>>,
     /// Interval
     pub interval: u64,
     /// Address of the influxdb service
@@ -39,6 +42,7 @@ impl Config {
     fn default() -> Config {
         Config {
             projects: vec![],
+            summaries: HashMap::new(),
             interval: 60,
             influx_host: "".to_string(),
             influx_bucket: "".to_string(),
@@ -50,6 +54,16 @@ impl Config {
     fn validate(&self) -> Result<(), &str> {
         if self.projects.is_empty() {
             return Err("projects can't be empty");
+        }
+        for (key, projects) in &self.summaries {
+            if projects.is_empty() {
+                return Err(format!("summary '{key}' is empty").leak());
+            }
+            for project in projects {
+                if !self.projects.contains(project) {
+                    return Err(format!("summary '{key}' contains unknown project '{project}'. Did you typo the name?").leak());
+                }
+            }
         }
         if self.interval < 5 {
             return Err("interval lower than 5s is very impolite.");
@@ -81,7 +95,7 @@ struct Stats {
     total_items_out: f64,
 }
 
-#[derive(Default, WriteDataPoint)]
+#[derive(Default, WriteDataPoint, Clone)]
 #[measurement = "downloader"]
 struct DownloaderPoint {
     #[influxdb(tag)]
@@ -96,7 +110,7 @@ struct DownloaderPoint {
     time: i64,
 }
 
-#[derive(Default, WriteDataPoint)]
+#[derive(Default, WriteDataPoint, Clone)]
 #[measurement = "project"]
 struct ProjectPoint {
     #[influxdb(tag)]
@@ -134,8 +148,9 @@ async fn main() {
 
     let config: Arc<Config>;
     match File::open("config.json") {
-        Ok(f) => match serde_json::from_reader(f) {
-            Ok(c) => {
+        Ok(f) => match serde_json::from_reader::<std::fs::File, Config>(f) {
+            Ok(mut c) => {
+                c.summaries.insert("summary-all".to_string(), c.projects.clone());
                 config = Arc::new(c);
             }
             Err(e) => {
@@ -165,16 +180,8 @@ async fn main() {
     let influx_client = Arc::new(InfluxClient::new(config.influx_host.clone(), config.influx_org.clone(), config.influx_api_token.clone()));
 
     let cancel = CancellationToken::new();
-    let mut join_set = JoinSet::new();
-    for project in &config.projects {
-        join_set.spawn(fetch(
-            project.clone(),
-            config.clone(),
-            cancel.clone(),
-            http_client.clone(),
-            influx_client.clone(),
-        ));
-    }
+
+    let worker = tokio::spawn(work(config, cancel.clone(), http_client, influx_client));
 
     match signal::ctrl_c().await {
         Ok(()) => {
@@ -186,85 +193,177 @@ async fn main() {
             exit(1);
         }
     }
-    while let Some(join) = join_set.join_next().await {
-        match join {
-            Ok(_) => {}
-            Err(err) => {
-                error!("Failed to join task: {}", err);
-            }
+    match worker.await {
+        Ok(_) => {}
+        Err(e) => {
+            error!("Failed to join worker: {e}");
         }
     }
+
     info!("Good bye o/")
 }
 
-async fn fetch(project: String, config: Arc<Config>, cancel: CancellationToken, http_client: Arc<HttpClient>, influx_client: Arc<InfluxClient>) {
+type StatsPerProject = (ProjectPoint, HashMap<String, DownloaderPoint>);
+
+fn add_project_stats(to: &mut StatsPerProject, times: &mut Vec<i64>, from: &StatsPerProject) {
+    to.0.items_done += from.0.items_done;
+    to.0.items_todo += from.0.items_todo;
+    to.0.items_out += from.0.items_out;
+    to.0.bytes += from.0.bytes;
+    times.push(from.0.time);
+    for (downloader, from_d_stats) in &from.1 {
+        if let Some(to_d_stats) = to.1.get_mut(downloader) {
+            to_d_stats.items += from_d_stats.items;
+            to_d_stats.bytes += from_d_stats.bytes;
+        } else {
+            to.1.insert(downloader.clone(), DownloaderPoint {
+                project: to.0.project.clone(),
+                downloader: downloader.clone(),
+                items: from_d_stats.items,
+                bytes: from_d_stats.bytes,
+                time: 0,
+            });
+        }
+    }
+}
+
+type ResultsMutex = Mutex::<HashMap<String, StatsPerProject>>;
+
+async fn work(config: Arc<Config>, cancel: CancellationToken, http_client: Arc<HttpClient>, influx_client: Arc<InfluxClient>) {
     loop {
         let start = Instant::now();
-        match http_client.get(format!("https://legacy-api.arpa.li/{project}/stats.json")).send().await {
-            Ok(res) => {
-                let now = Utc::now().timestamp_nanos();
-                if res.status().is_success() {
-                    match res.text().await {
-                        Ok(body) => {
-                            match serde_json::from_str::<Stats>(body.as_str()) {
-                                Ok(stats) => {
-                                    write_stats(stats, now, &project, &influx_client, &config.influx_bucket).await;
-                                    info!("Fetched & wrote stats for {project} in {:.2}s", start.elapsed().as_secs_f32())
-                                }
-                                Err(e) => {
-                                    println!("Failed to fetch {project} stats from tracker, could not parse json: {e}");
-                                }
-                            }
-                        }
-                        Err(e) => {
-                            error!("Failed to fetch {project} stats from tracker, could not read body: {e}");
-                        }
-                    }
-                } else {
-                    error!("Failed to fetch {project} stats from tracker, got bad status: {}", res.status().as_u16());
+        let results = Arc::new(ResultsMutex::new(HashMap::new()));
+        let mut join_set = JoinSet::new();
+        for project in &config.projects {
+            join_set.spawn(fetch(
+                project.clone(),
+                config.clone(),
+                http_client.clone(),
+                influx_client.clone(),
+                results.clone(),
+            ));
+        }
+        while let Some(join) = join_set.join_next().await {
+            match join {
+                Ok(_) => {}
+                Err(err) => {
+                    error!("Failed to join task: {}", err);
                 }
             }
-            Err(e) => {
-                error!("Failed to fetch {project} stats from tracker: {e}");
+        }
+        let results = results.lock().await;
+        for (summary, projects) in &config.summaries {
+            let mut stats = StatsPerProject::default();
+            stats.0.project = summary.clone();
+            let mut times = vec![];
+            let mut success = true;
+            for project in projects {
+                // these are expected to be here
+                let res = results.get(project);
+                if res.is_none() {
+                    info!("No result for project {project} while calculating summary {summary}, skipping");
+                    success = false;
+                    break;
+                }
+                add_project_stats(&mut stats, &mut times, res.unwrap());
             }
+            if !success {
+                continue;
+            }
+
+            let time = (times.iter().map(|&x| x as i128).sum::<i128>() / times.len() as i128) as i64;
+            stats.0.time = time;
+            let project_point = stats.0;
+            let mut downloader_points: Vec<DownloaderPoint> = stats.1.values().cloned().collect();
+            for dp in &mut downloader_points {
+                dp.time = time;
+            }
+            write_stats(project_point, downloader_points, &influx_client, &config.influx_bucket).await;
+            info!("Wrote stat summary for {summary}");
         }
         select! {
             _ = cancel.cancelled() => {
                 return;
             }
-            _ = sleep(Duration::from_secs(config.interval) - start.elapsed()) => {}
+            _ = sleep(Duration::from_secs(config.interval) - start.elapsed()) => {continue;}
         }
     }
 }
 
-async fn write_stats(stats: Stats, now: i64, project: &String, client: &Arc<InfluxClient>, bucket: &String) {
-    match client.write(bucket, stream::iter(vec![ProjectPoint {
+async fn fetch(project: String, config: Arc<Config>, http_client: Arc<HttpClient>, influx_client: Arc<InfluxClient>, results: Arc<ResultsMutex>) {
+    let start = Instant::now();
+    match http_client.get(format!("https://legacy-api.arpa.li/{project}/stats.json")).send().await {
+        Ok(res) => {
+            let now = Utc::now().timestamp_nanos();
+            if res.status().is_success() {
+                match res.text().await {
+                    Ok(body) => {
+                        match serde_json::from_str::<Stats>(body.as_str()) {
+                            Ok(stats) => {
+                                process_project_stats(stats, results, now, &project, &influx_client, &config.influx_bucket).await;
+                                info!("Fetched & wrote stats for {project} in {:.2}s", start.elapsed().as_secs_f32())
+                            }
+                            Err(e) => {
+                                println!("Failed to fetch {project} stats from tracker, could not parse json: {e}");
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        error!("Failed to fetch {project} stats from tracker, could not read body: {e}");
+                    }
+                }
+            } else {
+                error!("Failed to fetch {project} stats from tracker, got bad status: {}", res.status().as_u16());
+            }
+        }
+        Err(e) => {
+            error!("Failed to fetch {project} stats from tracker: {e}");
+        }
+    }
+}
+
+async fn process_project_stats(stats: Stats, results: Arc<ResultsMutex>, now: i64, project: &String, client: &Arc<InfluxClient>, bucket: &String) {
+    let project_point = ProjectPoint {
         project: project.clone(),
         items_done: stats.total_items_done as i64,
         items_todo: stats.total_items_todo as i64,
         items_out: stats.total_items_out as i64,
         bytes: stats.domain_bytes.iter().map(|f| *f.1 as i64).sum(),
         time: now,
-    }])).await {
-        Ok(_) => {}
-        Err(e) => {
-            error!("Failed to submit project {project} ProjectPoint to influx: {e}")
-        }
-    }
-    let mut points = vec![];
+    };
+    let mut downloader_points = vec![];
+    let mut downloader_points_map = HashMap::new();
     for dl in &stats.downloaders {
-        points.push(DownloaderPoint {
+        let p = DownloaderPoint {
             project: project.clone(),
             downloader: dl.clone(),
             items: *stats.downloader_count.get(dl).unwrap_or(&0f64) as i64,
             bytes: *stats.downloader_bytes.get(dl).unwrap_or(&0f64) as i64,
             time: now,
-        })
+        };
+        downloader_points.push(p.clone());
+        downloader_points_map.insert(dl.clone(), p);
     }
-    match client.write(bucket, stream::iter(points)).await {
+    {
+        let mut l = results.lock().await;
+        l.insert(project.clone(), (project_point.clone(), downloader_points_map));
+        drop(l);
+    }
+    write_stats(project_point, downloader_points, client, bucket).await;
+}
+
+async fn write_stats(project_point: ProjectPoint, downloader_points: Vec<DownloaderPoint>, client: &Arc<InfluxClient>, bucket: &String) {
+    match client.write(bucket, stream::iter(vec![project_point])).await {
         Ok(_) => {}
         Err(e) => {
-            error!("Failed to submit project {project} DownloaderPoints to influx: {e}")
+            error!("Failed to submit ProjectPoint to influx: {e}")
+        }
+    }
+
+    match client.write(bucket, stream::iter(downloader_points)).await {
+        Ok(_) => {}
+        Err(e) => {
+            error!("Failed to submit DownloaderPoints to influx: {e}")
         }
     }
 }
